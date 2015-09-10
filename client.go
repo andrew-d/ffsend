65c9b37ea59b5a50d14ea5f0e8485a8f36a115eb
Andrew Dunham <andrew@du.nham.ca>
2015-09-09 17:22:37 -0700

    Split ptls to its own package

--- client.go
+++ client.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/andrew-d/ffsend/ptls"
 	"github.com/andrew-d/id"
+	"github.com/andrew-d/ptls"
 )
 
 var clientCmd = &cobra.Command{
--- ptls/doc.go
+++
@@ -1,3 +0,0 @@
-// ptls implements a very simple TLS client/server that verify that the
-// certificate being presented by the remote end matches some given IDs.
-package ptls
